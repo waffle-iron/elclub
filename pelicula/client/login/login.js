abb5bf4df2f1a6788fd05ec832a9d3b781ae17bd
mariacelesterr <maria_rr_13@hotmail.com>
2016-09-25 21:54:28 -0400

    nuevas cosas

---
+++ pelicula/client/login/login.js
@@ -0,0 +1,35 @@
+
+Template.userManagement.events({  
+  'click #signup': function() {
+    var user = {
+      username: $('#signup-username').val(),
+      password: $('#signup-password').val(),
+      profile: {
+        fullname: $('#signup-fullname').val()
+      }
+    };
+
+    Accounts.createUser(user, function (error) {
+      if(error) alert(error);
+    });
+  }
+});
+
+Template.userManagement.events({  
+  'click #login': function() {
+    var username = $('#login-username').val();
+    var password = $('#login-password').val();
+
+    Meteor.loginWithPassword(username, password, function(error) {
+      if(error) alert(error);
+    });
+  }
+});
+
+
+Template.userManagement.events({  
+	'click #logout': function() {  
+  	Meteor.logout();
+}
+  
+});
